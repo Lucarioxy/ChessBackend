2206763888fb74cd71f007e3a2372209cc541529
chetan2332 <101946181+chetan2332@users.noreply.github.com>
2024-03-23 19:19:03 +0530

    intialized the sockets

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+const { Server } = require('socket.io');
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
